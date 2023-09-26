6e5be1e02c38bd7aa59b45505f64e7d6783ad536
agtz92 <alfredo.gtz92@gmail.com>
2023-09-25 23:40:30 -0600

    isMobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Nav from "@/components/Nav"
 import "@/styles/globals.css"
+import { useMediaQuery } from "@mui/material"
 
 export default function App({ Component, pageProps }) {
+  const isMobile = useMediaQuery('(max-width:768px)');
   return (
     <>
       <Nav />
-      <Component {...pageProps} />
+      <Component {...pageProps} isMobile={isMobile} />
     </>
   )
 }
