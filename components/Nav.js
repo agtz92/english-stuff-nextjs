7a315f012594903a5049f772ffbbc1f416d1d7cc
agtz92 <alfredo.gtz92@gmail.com>
2023-09-25 22:57:58 -0600

    NavBar

---
+++ components/Nav.js
@@ -0,0 +1,37 @@
+// components/Nav.js
+import Link from "next/link"
+import { useState } from "react"
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <nav className={`nav ${isOpen ? "open" : ""}`}>
+      <div className="logo">
+        <Link href="/">Logo</Link>
+      </div>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`menu ${isOpen ? "open" : ""}`}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
