1a2ff2513af92e5aa38d2d9562efe0ea264935d5
agtz92 <alfredo.gtz92@gmail.com>
2023-09-27 22:56:18 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Link from "next/link"
 import Head from "next/head"
 import { Box, Grid } from "@mui/material"
 import LargeCard from "@/components/LargeCard"
-import {sitename} from "../components/siteData"
+import { sitename } from "../components/siteData"
 
 export default function Home({ blogs, isMobile }) {
   // Sort the blogs by date in descending order
   const sortedBlogs = blogs.sort((a, b) => new Date(b.date) - new Date(a.date))
-
+  const firstTwo = sortedBlogs.slice(0, 2);
+  const nextSix = sortedBlogs.slice(2, 8);
+  const theRest = sortedBlogs.slice(8);
   return (
     <Box
       sx={{ marginLeft: !isMobile ? 20 : 5, marginRight: !isMobile ? 20 : 5 }}
@@ -17,12 +19,37 @@ export default function Home({ blogs, isMobile }) {
       <Head>
         <title>{sitename}</title>
       </Head>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Grid container>
+            {firstTwo.map((blog) => (
+              <Grid key={blog.slug} item xs={12} md={12}>
+                <Link href={`/${blog.slug}`}>
+                  <LargeCard post={blog} height="500px" />
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Grid container>
+            {nextSix.map((blog) => (
+              <Grid key={blog.slug} item xs={12} md={12}>
+                <Link href={`/${blog.slug}`}>
+                  <LargeCard post={blog} height="500px" />
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={4}></Grid>
+      </Grid>
 
       <Grid container spacing={2}>
         {sortedBlogs.map((blog) => (
           <Grid key={blog.slug} item xs={12} md={3}>
             <Link href={`/${blog.slug}`}>
-              <LargeCard post={blog} />
+              <LargeCard post={blog} height="600px" />
             </Link>
           </Grid>
         ))}
