3d80acb09c887b5fa5b0932d4b52965d5048d8ad
agtz92 <alfredo.gtz92@gmail.com>
2023-09-30 12:19:42 -0600

    google ads

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ export default function Document() {
           async
           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7182528185795867"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
